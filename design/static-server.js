dfdb317410e1ed79a5297a99b459110e12a00ef9
Tony Tam <fehguy@gmail.com>
2013-10-22 23:08:31 -0700

    added swagger-ui

--- design/static-server.js
+++ design/static-server.js
@@ -3,6 +3,7 @@ var app = express();
 app.use(express.bodyParser());
 
 var docs_handler = express.static(__dirname + '/specs');
+var ui_handler = express.static(__dirname + '/swagger-ui');
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -24,6 +25,16 @@ app.get(/^\/docs(\/.*)?$/, function(req, res, next) {
   }
   req.url = req.url.substr('/docs'.length);
   return docs_handler(req, res, next);
+});
+
+app.get(/^\/swagger-ui(\/.*)?$/, function(req, res, next) {
+  if (req.url === '/swagger-ui') {
+    res.writeHead(302, { 'Location' : req.url + '/' });
+    res.end();
+    return;
+  }
+  req.url = req.url.substr('/swagger-ui'.length);
+  return ui_handler(req, res, next);
 });
 
 app.listen(8000);
