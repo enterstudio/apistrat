eb434327eaf5833e15810a7fdf1fdd3b0b7e0a1b
Tony Tam <fehguy@gmail.com>
2013-10-22 22:49:33 -0700

    added files

--- client/objc-client/Sample/Sample/main.m
+++ client/objc-client/Sample/Sample/main.m
@@ -9,10 +9,23 @@
 #import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
+#import "SWGEventsApi.h"
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
+SWGEventsApi * api = [[SWGEventsApi alloc] init];
+[api findEventByIdWithCompletionBlock:@123
+                    completionHandler:^(SWGEvent *event, NSError *error) {
+                        if(error) {
+                            // handle it
+                        }
+                        else {
+                            // do something with the SWGEvent
+                            NSLog(@"%@", [event asDictionary]);
+                        }
+                    }];
+        
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
